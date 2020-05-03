d427f1694ed7aed707f7c1b456f586add18d04b8
cale <caleanderson9@gmail.com>
2020-05-03 11:48:07 -0500

    add anderson controller and index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/names" => "names#index"
     get "/mcdonald" => "mcdonald#index"
-    get "/computers" => "computers#index"
+    get "/anderson" => "anderson#index"
     get "/branch" => "waszak#index"
     get "/pages" => "pages#index"
     get "/kamvazaanas" => "kamvazaana#index"
